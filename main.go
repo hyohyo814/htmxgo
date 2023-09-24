22e331710631b53c7dc3070a9be4c84caf7e5b56
hyohyo814 <sojaisam@gmail.com>
2023-09-23 23:27:23 -0700

    Hello

--- main.go
+++ main.go
@@ -7,15 +7,28 @@ import (
 	"text/template"
 )
 
+type Film struct {
+	Title    string
+	Director string
+}
+
 func main() {
 	fmt.Println("hello world")
 
 	h1 := func(w http.ResponseWriter, r *http.Request) {
 		tmpl := template.Must(template.ParseFiles("index.html"))
-		tmpl.Execute(w, nil)
+		films := map[string][]Film{
+			"Films": {
+				{Title: "The Godfather", Director: "Francis Ford Coppola"},
+				{Title: "Blade Runner", Director: "Ridley Scott"},
+				{Title: "The Thing", Director: "John Carpenter"},
+			},
+		}
+
+		tmpl.Execute(w, films)
 	}
 
 	http.HandleFunc("/", h1)
 
-log.Fatal(http.ListenAndServe(":8000", nil))
+	log.Fatal(http.ListenAndServe(":8000", nil))
 }
